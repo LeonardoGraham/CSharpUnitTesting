44e890316eef840a357365e7048a3ef3657dec8a
LeonardoGraham <leonardostefangraham@gmail.com>
2021-10-07 15:14:41 -0400

    Created unit test for CustomerController class.

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        private CustomerController _controller;
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new CustomerController();
+        }
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var result = _controller.GetCustomer(0);
+
+            // Asserts that result is of type NotFound
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //// Asserts that result is of type NotFound or one of its derivaties
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        { }
+    }
+}
