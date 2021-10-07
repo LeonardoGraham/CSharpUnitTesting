96b37491a57c8e5ce4047a915c1383c9f1f6234f
LeonardoGraham <leonardostefangraham@gmail.com>
2021-10-07 14:30:50 -0400

    Added UnitTest for HtmlFormatter class.

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.Equals("abc");
+
+            // Assertion Types
+            // Specific 
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            //// More general
+            //Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            //Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            //Assert.That(result, Does.Contain("abc").IgnoreCase);
+
+        }
+    }
+}
