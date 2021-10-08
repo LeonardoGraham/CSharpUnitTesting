91ebd6c1e7b943eb2511cf27e8799b1e8cbe432c
LeonardoGraham <leonardostefangraham@gmail.com>
2021-10-08 01:53:29 -0400

    Added mock tests for VideoService class. Used dependency injection via constructor.

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var service = new VideoService(new FakeFileReader());
+
+            var result = service.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
