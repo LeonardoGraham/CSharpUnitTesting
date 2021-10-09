33568b33df649ae189b23d59676861d565412c94
Leonardo Graham <leonardostefangraham@gmail.com>
2021-10-09 19:59:54 -0400

    Created unit tests for InstallerHelper class.

--- TestNinja/Mocking/InstallerHelper.cs
+++ TestNinja/Mocking/InstallerHelper.cs
@@ -4,17 +4,22 @@ namespace TestNinja.Mocking
 {
     public class InstallerHelper
     {
+        private IFileDownloader _fileDownloader;
         private string _setupDestinationFile;
+
+        public InstallerHelper(IFileDownloader fileDownloader = null)
+        {
+            _fileDownloader = fileDownloader ?? new FileDownloader();
+        }
 
         public bool DownloadInstaller(string customerName, string installerName)
         {
-            var client = new WebClient();
             try
             {
-                client.DownloadFile(
+                _fileDownloader.DownloadFile(
                     string.Format("http://example.com/{0}/{1}",
-                        customerName,
-                        installerName),
+                    customerName,
+                    installerName),
                     _setupDestinationFile);
 
                 return true;
